65c01ccdbb15ce5118f5114e5e3be046b0b921c4
Stenstromen <kontakt@filipstenstrom.se>
2024-11-26 00:30:59 +0100

    Updated server version

--- doh-server/version.go
+++ doh-server/version.go
@@ -1,6 +1,6 @@
 package main
 
 const (
-	VERSION    = "2.3.7"
-	USER_AGENT = "DNS-over-HTTPS/" + VERSION + " (+https://github.com/m13253/dns-over-https)"
+	VERSION    = "1.0.0"
+	USER_AGENT = "DNS-over-HTTPS/" + VERSION + " (+https://github.com/stenstromen/dns-over-https)"
 )
